585a5850160af92365c7e9f97815e2a40a2995ad
yangxs <yangxs@xiaopeng.com>
2023-10-12 19:15:33 +0800

    feat: 定时任务方法添加

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -1,9 +1,11 @@
 const schedule = require('node-schedule');
+const weatherScheduleJob = require('./weatherSchedule');
 
 const scheduleJob = () => {
-  
-  schedule.scheduleJob('0 0 8 * * *', () => {
-    console.log('scheduleCronstyle:' + new Date());
+
+  schedule.scheduleJob('0 0 8 * * ?', () => {
+    // console.log('scheduleCronstyle:' + new Date());
+    weatherScheduleJob();
   });
 };
 module.exports = scheduleJob;
---
+++ src/schedule/weatherSchedule/index.ts
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const weatherSchedule = () => {
+  
+};
+module.exports = weatherSchedule;
